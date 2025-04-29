7c6d0a35245fac26050d6964df1ffa04e4a22034
slouch2 <minchil295@gmail.com>
2025-04-29 20:48:55 +0900

    Delete utils directory

--- utils/telegram_bot.py
+++
@@ -1,9 +0,0 @@
-# utils/telegram_bot.py
-
-import telegram
-from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
-
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
-
-async def send_telegram_message(message):
-    await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
--- utils/time_utils.py
+++
@@ -1,8 +0,0 @@
-# utils/time_utils.py
-
-from datetime import datetime, timezone, timedelta
-
-def get_current_time():
-    kst = timezone(timedelta(hours=9))
-    now = datetime.now(kst)
-    return now.strftime("%Y-%m-%d %H:%M:%S")
